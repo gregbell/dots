725e98195772863042b2d48377e87d54f33fa7b6
Greg Bell <code@gregbell.ca>
2026-06-13 02:43:30 +0200

    fzf: default use exact matching

--- base/.config/zsh/conf.d/11-fzf.zsh
+++ base/.config/zsh/conf.d/11-fzf.zsh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_OPTS="--style minimal --color 16 --layout=reverse --height 30% --preview='bat -p --color=always {}'"
-export FZF_CTRL_R_OPTS="--style minimal --color 16 --info inline --no-sort --no-preview" # separate opts for history widget
+export FZF_DEFAULT_OPTS="--style minimal --color 16 -e --layout=reverse --height 30% --preview='bat -p --color=always {}'"
+export FZF_CTRL_R_OPTS="--style minimal --color 16 -e --info inline --no-sort --no-preview" # separate opts for history widget
 
 source <(fzf --zsh)
